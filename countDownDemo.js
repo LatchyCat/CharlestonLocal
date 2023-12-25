0353c462cf2e522e8b3f20c8f4204612d2ac2247
oriel <orielsilvajr@gmail.com>
2023-12-24 21:59:21 -0500

    site test js complete

--- countDownDemo.js
+++ countDownDemo.js
@@ -1,16 +1,16 @@
-// Set the date we're counting down to
+// Set the date we're counting down to (January 1, 2024 00:00:00)
 var countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
 
-    // Get todays date and time
+    // Get today's date and time
     var now = new Date().getTime();
 
-    // Find the distance between now an the count down date
+    // Find the distance between now and the count down date
     var distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
+    // Time calculations for days, hours, minutes, and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -26,3 +26,4 @@ var x = setInterval(function () {
         document.getElementById("demo").innerHTML = "EXPIRED";
     }
 }, 1000);
+
